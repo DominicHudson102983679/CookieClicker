68ef59834cac7ddce4ee1a4ef9a68ae166110245
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-04-24 13:32:58 +1000

    add html upgrades, started auto clicker

--- cookie.js
+++ cookie.js
@@ -1,13 +1,30 @@
 var cookieCount = document.getElementById('cookieCount');                               
-var count = 0;                                                                          
+var score = 0;                                                                          
 var cookieImage = document.getElementById('cookieImage');                               
-
+var autoClick = document.getElementById('autoClick');
+var tripleClick = document.getElementById('tripleClick');
 
 function onClick(e) {   
     e.preventDefault();                                                               
-    count++;                                                                            
-    cookieCount.innerHTML = count + " Cookies";                             
+    score++;                                                                            
+    cookieCount.innerHTML = score + " Cookies";                             
 };                                                                                      
 
 cookieImage.addEventListener('click', onClick);
+var hasAutoClick = false;
+autoClick.addEventListener('click',  function() {
 
+
+    //bug fixed 1 line below - use >= instead of >
+   if (score >= 10 && hasAutoClick === false) {
+     setInterval(function() {
+       score++;
+       cookieCount.textContent = score;
+     }, 500);
+     this.classList.add("disabled", "disabled");
+     this.textContent = "Purchased";
+     hasAutoClick = true;
+   } else if (score < 50 && hasAutoClick === false) {
+     alert("You don't have enough cookies!");
+   }
+ })
