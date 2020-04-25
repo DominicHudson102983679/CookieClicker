6aad36fff67e999736280dc243dfa24b1b225a7c
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-04-25 13:39:53 +1000

    triple click function added

--- cookie.js
+++ cookie.js
@@ -16,7 +16,7 @@ var tripleClickFunc = false;
 
 
 // single click function
-function onClick(e) {   
+function plusOne(e) {   
     // prevent page from resetting after click
     e.preventDefault();                                                               
     // cookie count (score) increases by 1 each click
@@ -26,11 +26,12 @@ function onClick(e) {
 };                                                                                      
 
 // attaching a single click listener function to the cookie image
-cookieImage.addEventListener('click', onClick);
+cookieImage.addEventListener('click', plusOne);
 
 
 // attaching a 'click' listening function to the auto-clicker button
 autoClick.addEventListener('click', function() {
+
     // if user has 10 cookies or more AND the auto-clicker is off, run this function
     if (score >= 10 & autoClickFunc == false) {
       // continuously repeats this function by adding 1 cookie every .5 seconds
@@ -38,9 +39,8 @@ autoClick.addEventListener('click', function() {
        score++;
        cookieCount.innerHTML = score + " Cookies";
      }, 500);
-     // change auto-click upgrade box to purchased
      autoClick.innerHTML = "Purchased";
-     // changes auto-click variable to true
+     // turns auto-clicker on
      autoClickFunc = true;
     }
      // if user has less than 10 cookies AND the auto-click function is off, alert user
@@ -52,24 +52,32 @@ autoClick.addEventListener('click', function() {
 
 // attaching a 'click' listening function to the triple-clicker button
 tripleClick.addEventListener('click', function() {
+
     // if user has 10 cookies or more AND the auto-clicker is off, run this function
-    if (score >= 25 & tripleClickFunc == false & singleClick == true ) {
- 
-      function plusThree() {
-        
-        score += 3;
-        cookieCount.innerHTML = score + " Cookies";
-      };
+    if (score >= 25 && tripleClickFunc == false) {
+        function plusThree(e) {   
+        // prevent page from resetting after click
+                                                             
+        // cookie count (score) increases by an additional 2 each click (totalling 3 per click)
+        score += 2;
+        // changes the initial cookie count text to "# cookies"                                                                            
+        cookieCount.innerHTML = score + " Cookies";                             
+    };
+
+// another listening event is added to the cookie image. functions plusOne and plusThree now both active    
+cookieImage.addEventListener('click', plusThree);
+
+
      // change triple-click upgrade box to purchased
      tripleClick.innerHTML = "Purchased";
      // changes triple-click variable to true
-     tripleClickFunc = true;
-        singleClick = false;
+     /////// tripleClickFunc = true;
+
       }
       
      // if user has less than 25 cookies AND the triple-click function is off, alert user
     
-     else if(score < 25 & tripleClickFunc == false) {
+     else if (score < 25 & tripleClickFunc == false) {
      alert("Need more cookies!");
    }
  }
